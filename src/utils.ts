591b6d56df3cb04ef119fcc2dd178f4b24614efc
wasabina67 <wasabina67@gmail.com>
2024-08-31 12:50:07 +0900

    Create utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,12 @@
+export const calculateAge = (dateString: string): number => {
+  const birthDate = new Date(dateString);
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDifference = today.getMonth() - birthDate.getMonth();
+
+  if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+
+  return age;
+};
